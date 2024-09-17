a7342b8df4129ad40aabbd7e4961d74a7a68bdb8
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2024-09-17 23:06:49 +0500

    add black section

---
+++ src/components/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div className='bg-[#2d2d2d] text-white text-center py-14'>
+      <h1 className='text-6xl font-bold'>Save big with our cheap car rental!</h1>
+      <p className='text-2xl tracking-wide font-medium mt-5'>Top Airports. Local Suppliers. <span className='text-[#FF4C38]'>24/7</span> Support.</p>
+    </div>
+  )
+}
+
+export default Banner
--- src/pages/Frontend/Home.jsx
+++ src/pages/Frontend/Home.jsx
@@ -4,16 +4,18 @@ import Hero from '../../components/Hero'
 import BookACar from '../../components/BookACar'
 import Quick from '../../components/Quick'
 import BackToTop from '../../components/BackToTop'
+import Banner from '../../components/Banner'
 function Home() {
   return (
     <div className="hero-bg">
       <BackToTop></BackToTop>
     <Navbar></Navbar>
-    <div className="w-[95%] mx-auto">
+    <div className="2xl:w-[65%] w-[95%] mx-auto">
       <Hero></Hero> 
       <BookACar></BookACar>
       <Quick></Quick>
     </div>
+      <Banner></Banner>
     </div>
   )
 }
