82d4aa1401c4b345e98492defebcb7537d6ac6b0
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2024-09-23 00:39:19 +0500

    add about img

--- src/pages/Frontend/About.jsx
+++ src/pages/Frontend/About.jsx
@@ -6,7 +6,13 @@ import Quick from '../../components/Quick'
 function About() {
   return (
     <>
+    <div id='heros-bg'>
     <Navbar></Navbar>
+    <div className='ml-[2.5%] mt-16'>
+    <h2 className='font-extrabold text-4xl'>About</h2>
+    <h3 className='text-black font-semibold mt-4'>Home / About</h3>
+    </div>
+    </div>
     <Quick></Quick>
     <BookBanner></BookBanner>
     <Footer></Footer>
